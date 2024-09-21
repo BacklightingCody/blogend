390e5924729bf6c7561cc6bf7dfa579eb5288fe8
backlighting <pitilessmiracle@outlook.com>
2024-09-21 21:39:31 +0800

    chore:简单构建auth模块并与前端联调

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(); // 可以根据需要添加管道
+  app.enableCors(); // 配置 CORS
+  await app.listen(8000);
 }
+
 bootstrap();
