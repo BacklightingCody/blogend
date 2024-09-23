2d3a01a57f4120c143277c10113eac5c4ba52cee
backlighting <pitilessmiracle@outlook.com>
2024-09-23 22:50:28 +0800

    feat初步:引入拦截器和过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
+import { HttpExceptionFilter } from '@/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(); // 可以根据需要添加管道
   app.enableCors(); // 配置 CORS
   await app.listen(8000);
